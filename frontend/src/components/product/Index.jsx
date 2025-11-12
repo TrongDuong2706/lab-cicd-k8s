c0f58ee60729d39be1e0e9a6cf612211aec2d822
Trong Duong <trongdeptrai57@gmail.com>
2025-11-12 22:40:05 +0700

    test pipeline

--- frontend/src/components/product/Index.jsx
+++ frontend/src/components/product/Index.jsx
@@ -46,7 +46,7 @@ export default function ProductIndex(props) {
               )}
             </tbody>
           </table>
-          <Link className="btn btn-primary" to="/product/create">Createss</Link>
+          <Link className="btn btn-primary" to="/product/create">Create</Link>
         </div>
       </div>
     </div>
