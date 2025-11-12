a33c08ea419a57d0c352fee372823e8cd542324e
Trong Duong <trongdeptrai57@gmail.com>
2025-11-12 21:50:28 +0700

    test argo

--- infra/locals.tf
+++ infra/locals.tf
@@ -2,9 +2,19 @@ locals {
   full_jenkins_domain_name = "${var.jenkins_subdomain}.${var.domain_name}"
   full_argocd_domain_name  = "${var.argocd_subdomain}.${var.domain_name}"
 
-  route53_records = {
-    app     = var.domain_name
-    jenkins = local.full_jenkins_domain_name
-    argocd  = local.full_argocd_domain_name
+  env_records = {
+    dev  = "dev.${var.domain_name}"
+    uat  = "uat.${var.domain_name}"
+    prod = var.domain_name  
   }
+
+  # Tổng hợp tất cả records
+  route53_records = merge(
+    {
+      app     = var.domain_name
+      jenkins = local.full_jenkins_domain_name
+      argocd  = local.full_argocd_domain_name
+    },
+    local.env_records
+  )
 }
