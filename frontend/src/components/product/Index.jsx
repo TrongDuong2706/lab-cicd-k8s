0e2cdb5d9cd6d36f424a1f094c9991278818b426
Trong Duong <trongdeptrai57@gmail.com>
2025-11-13 08:46:37 +0700

    test argo

--- frontend/src/components/product/Index.jsx
+++ frontend/src/components/product/Index.jsx
@@ -46,7 +46,7 @@ export default function ProductIndex(props) {
               )}
             </tbody>
           </table>
-          <Link className="btn btn-primary" to="/product/create">Createsss</Link>
+          <Link className="btn btn-primary" to="/product/create">Create</Link>
         </div>
       </div>
     </div>
